190e54476dd5f381b0cf865baa76c52ca94e947c
MohsenBg <moh.138.1393@gmail.com>
2022-11-05 11:49:41 +0330

    two_sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod reverse_vowels_of_a_string;
-
+mod two_sum;
 fn main() {
-    let a = reverse_vowels_of_a_string::Solution::reverse_vowels("leetcode".to_string());
-    println!("{}", a)
+    let nums: Vec<i32> = vec![2, 7, 11, 15];
+    let target: i32 = 9;
+    let a = two_sum::Solution::solution_2(nums, target);
+    dbg!(a);
 }
--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,3 +1,33 @@
+#[derive(Debug)]
+pub struct Solution {}
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {}
+    pub fn solution_1(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut i = 0;
+        while i < nums.len() {
+            let mut j = i + 1;
+            while j < nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        return vec![];
+    }
+
+    pub fn solution_2(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut n: i32 = nums.len() as i32 - 1;
+        let mut nums = nums;
+        while n >= 0 {
+            let t: i32 = target - nums.pop().unwrap();
+            for (index, value) in nums.iter().enumerate() {
+                if *value == t {
+                    return vec![index as i32, n];
+                }
+            }
+            n -= 1;
+        }
+        vec![]
+    }
 }
