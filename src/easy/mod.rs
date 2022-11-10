7cba3862d5c296032a829043d88b5e0b0e9c6b51
MohsenBg <moh.138.1393@gmail.com>
2022-11-10 17:25:13 +0330

    remove_all_adjacent_duplicates_in_string

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -2,6 +2,7 @@ mod longest_common_prefix;
 mod make_the_string_great;
 mod maximum_69_number;
 mod palindrome_number;
+mod remove_all_adjacent_duplicates_in_string;
 mod reverse_vowels_of_a_string;
 mod roman_to_integer;
 mod two_sum;
---
+++ src/easy/remove_all_adjacent_duplicates_in_string.rs
@@ -0,0 +1,53 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(s: String) -> String {
+        if s.len() <= 1 {
+            return s;
+        }
+        let mut index: usize = 0;
+        let mut s: Vec<char> = s.chars().collect();
+
+        while s.len() > 1 && index < s.len() - 1 {
+            if s[index] == s[index + 1] {
+                s.remove(index);
+                s.remove(index);
+                if index > 0 {
+                    index -= 1;
+                }
+                continue;
+            }
+
+            index += 1;
+        }
+        s.iter().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::Solution;
+    #[test]
+    fn Solution_1_test_1() {
+        let input = "abbaca".to_string();
+        let expected = "ca".to_string();
+        let res = Solution::remove_duplicates(input);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn Solution_1_test_2() {
+        let input = "azxxzy".to_string();
+        let expected = "ay".to_string();
+        let res = Solution::remove_duplicates(input);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn Solution_1_test_3() {
+        let input = "aaaaaaaaa".to_string();
+        let expected = "".to_string();
+        let res = Solution::remove_duplicates(input);
+    }
+}
