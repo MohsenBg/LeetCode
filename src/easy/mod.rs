7f94291082ff546a1675e4413748cc3800ff39bf
MohsenBg <moh.138.1393@gmail.com>
2022-11-11 09:05:22 +0330

    remove_duplicates_from_sorted_array

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -3,6 +3,7 @@ mod make_the_string_great;
 mod maximum_69_number;
 mod palindrome_number;
 mod remove_all_adjacent_duplicates_in_string;
+mod remove_duplicates_from_sorted_array;
 mod reverse_vowels_of_a_string;
 mod roman_to_integer;
 mod two_sum;
---
+++ src/easy/remove_duplicates_from_sorted_array.rs
@@ -0,0 +1,44 @@
+use std::collections::HashSet;
+
+pub struct Solution {}
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut used_nums: HashSet<i32> = HashSet::new();
+        let mut index = 0;
+        while index < nums.len() {
+            match used_nums.get(&nums[index]) {
+                None => {
+                    used_nums.insert(nums[index]);
+                }
+                Some(_) => {
+                    nums.remove(index);
+                    continue;
+                }
+            }
+            index += 1;
+        }
+        nums.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn Solution_1_test_1() {
+        let mut input: Vec<i32> = vec![1, 1, 2];
+        let expected: i32 = 2;
+        let res = Solution::remove_duplicates(&mut input);
+        assert_eq!(res, expected);
+        assert_eq!(vec![1, 2], input);
+    }
+
+    #[test]
+    fn Solution_1_test_2() {
+        let mut input: Vec<i32> = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        let expected: i32 = 5;
+        let res = Solution::remove_duplicates(&mut input);
+        assert_eq!(res, expected);
+        assert_eq!(vec![0, 1, 2, 3, 4], input);
+    }
+}
